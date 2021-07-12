7bb6b225b0db3da9070dd5211724921602774f3a
Your Name <you@example.com>
2021-07-12 16:48:42 +0100

    dump

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pistorm-video-passtrough
+Dump for the current video passtrough.
+!This is not ready for production yet!
---
+++ i2c_loader/CMakeFiles/i2c_updater.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/i2c_updater.dir/src/bitstream.cpp.o"
+  "CMakeFiles/i2c_updater.dir/src/i2c.cpp.o"
+  "CMakeFiles/i2c_updater.dir/src/main.cpp.o"
+  "i2c_updater"
+  "i2c_updater.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/i2c_updater.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
